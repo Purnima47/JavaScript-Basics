b2b5b32c34c40920e7318c4527c008b8aadb012b
Purnima Kesarwani <purnimakesarwani47@gmail.com>
2024-09-03 00:51:29 +0530

    js basics

--- object.js
+++ object.js
@@ -9,7 +9,29 @@ let details = {
 // console.log(details);
 // console.log(details['name']); // or details.name
 
+details['country'] = "India";
+console.log(details);
+
+// delete obj keys and obj itself
+delete details.country;
+
+
 //  for in loop
 for (const key in details) {
     console.log(key, details[key]);
 }
+
+console.log(Object.keys(details));
+console.log(Object.values(details));
+
+// Get both key and values
+console.log(Object.entries(details));
+
+// Cloning an object using assign
+
+const new_details = Object.assign({}, details);
+console.log(new_details);
+
+// cloning with some additional features
+const new_details_1 = Object.assign({}, details, { role: "Developer" });
+console.log(new_details_1);
