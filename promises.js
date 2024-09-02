95a95f0483e5dca02551163e4d7bf06079eac13e
Purnima Kesarwani <purnimakesarwani47@gmail.com>
2024-09-03 02:59:20 +0530

    js basics

---
+++ promises.js
@@ -0,0 +1,61 @@
+// JS -> SINGLE THREADED.
+
+const at = new Promise((resolve, reject) => {
+    let decide = true;
+
+    if (decide) {
+        // to pass more than one parameter we need to pass it as objects.
+        resolve({ msg: ":-)", decision: decide });
+    } else {
+        reject({ msg: ":-(", decision: decide });
+    }
+});
+
+// actions on the promise
+
+at.then(({ msg, decision }) => {
+    console.log("Msg ", msg + " Decision", decision);
+    console.log("Iphone mil gya");
+}).catch(({ msg, decision }) => {
+    console.log("Msg ", msg + "Decision", decision);
+    console.log("Purana phone hi best tha");
+}).finally(() => console.log("......"));
+
+
+const f1Promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (Math.random() > 0.5) {
+            resolve("Friend 1: We will do party");
+        } else {
+            reject("Friend 1: Have work");
+        }
+    }, 3000);
+});
+
+const f2Promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (Math.random() > 0.5) {
+            resolve("Friend 2: We will do party");
+        } else {
+            reject("Friend 2: Headache");
+        }
+    }, 3000);
+});
+
+const f3Promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (Math.random() > 0.5) {
+            resolve("Friend 3: We will do party");
+        } else {
+            reject("Friend 3: Abba nhi manenge");
+        }
+    }, 3000);
+});
+
+Promise.all([f1Promise, f2Promise, f3Promise]).then((msg) => {
+    console.log("Msg: ", msg);
+    console.log("Yeah!");
+}).catch((msg) => {
+    console.log("Msg: ", msg);
+    console.log(":-(");
+})
